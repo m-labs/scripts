b3e6c3331ca526fd58cc2f4bbf6d3d6d6ca883e7
Xiangfu Liu <xiangfu@sharism.cc>
2011-04-10 20:18:52 +0800

    reflash_m1.sh: reflash script for m1, reflash all

---
+++ reflash_m1.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+# version of me
+__VERSION__="2011-04-10"
+
+BATCH_FILE=flash.batch
+NOVERIFY=noverify
+
+batch() {
+    echo -e "$1" >> "${BATCH_FILE}"
+}
+
+rm -rf ${BATCH_FILE}
+batch "cable milkymist"
+batch "detect"
+batch "instruction CFG_OUT 000100 BYPASS"
+batch "instruction CFG_IN 000101 BYPASS"
+batch "pld load fjmem.bit"
+batch "initbus fjmem opcode=000010"
+batch "frequency 6000000"
+batch "detectflash 0"
+batch "endian big"
+
+batch "flashmem 0x000000 standby.fpg ${NOVERIFY}"
+
+batch "flashmem 0x0A0000 soc-rescue.fpg ${NOVERIFY}"
+batch "flashmem 0x220000 bios-rescue.bin ${NOVERIFY}"
+batch "flashmem 0x240000 splash-rescue.raw ${NOVERIFY}"
+
+batch "flashmem 0x6E0000 soc.fpg ${NOVERIFY}"
+batch "flashmem 0x860000 bios.bin ${NOVERIFY}"
+batch "flashmem 0x880000 splash.raw ${NOVERIFY}"
+
+batch "flashmem 0x920000 flickernoise.fbiz ${NOVERIFY}"
+
+batch "eraseflash 0xD20000 151"
+batch "flashmem   0xD20000 data.flash5.bin ${NOVERIFY}"
+
+jtag ${BATCH_FILE}
