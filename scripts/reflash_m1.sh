8b4cb9b0edda9542d2f64897cf0a4939b86ce1c4
Xiangfu Liu <xiangfu@sharism.cc>
2011-08-29 21:32:52 +0800

    reflash_m1.sh merge reflash_m1_rc3.sh

--- scripts/reflash_m1.sh
+++ scripts/reflash_m1.sh
@@ -21,12 +21,20 @@ call-help() {
 	echo "
 Usage: ./reflash_m1.sh                    version: ${__VERSION__}
 	--release [VERSION]          # by default it will download 'currect' release
+                                     # URL: http://milkymist.org/updates/
+
 	--snapshot <VERSION> [data]  # if 'data' enable will reflash data partitions
+                                     # URL: http://fidelio.qi-hardware.com/~xiangfu/build-milkymist/
+
 	--local-folder <PATH>        # all files must be under <PATH>
+
 	--lock-flash                 # lock 'standby' and 'rescue' partitions
+
 	--read-flash                 # be default read 'standby.bin' from m1
+
 	--bios-mac 00 2a             # '00' '2a' is the last MAC address
 	--rc3 00 2a                  # used in factory flash
+                                     # --bios-mac and --rc3 needs 'mkmmimg'
 
 Written by: Xiangfu Liu <xiangfu@sharism.cc>
 Please report bugs to <devel@lists.milkymist.org>
@@ -56,6 +64,11 @@ call-jtag() {
     if [ ! -f "${FJMEM_PATH}/${FJMEM}" ]; then
 	wget -O "${FJMEM_PATH}/${FJMEM}" http://milkymist.org/updates/2011-07-13/for-rc3/fjmem.bit
     fi
+
+    if [ "${BIOS_RESCUE_PATH}" == "" ]; then
+	BIOS_RESCUE_PATH=${WORKING_DIR}
+    fi
+
 
     # UrJtag option ##########################################
     JTAG_DEBUG=""
@@ -90,7 +103,7 @@ EOF
 	echo "readmem 0x000000 0x00A0000 ${WORKING_DIR}/${STANDBY}" >> ${JTAG_BATCH_FILE}
 
 	#echo "readmem 0x0A0000 0x0180000 ${WORKING_DIR}/${SOC_RESCUE}" >> ${JTAG_BATCH_FILE}
-	#echo "readmem 0x220000 0x0020000 ${WORKING_DIR}/${BIOS_RESCUE}" >> ${JTAG_BATCH_FILE}
+	#echo "readmem 0x220000 0x0020000 ${BIOS_RESCUE_PATH}/${BIOS_RESCUE}" >> ${JTAG_BATCH_FILE}
 	#echo "readmem 0x240000 0x00A0000 ${WORKING_DIR}/${SPLASH_RESCUE}" >> ${JTAG_BATCH_FILE}
 	#echo "readmem 0x2E0000 0x0400000 ${WORKING_DIR}/${FLICKERNOISE_RESCUE}" >> ${JTAG_BATCH_FILE}
 
@@ -107,7 +120,7 @@ EOF
 
 	echo "flashmem 0x000000 ${WORKING_DIR}/${STANDBY} ${JTAG_NOVERIFY}" >> ${JTAG_BATCH_FILE}
 	echo "flashmem 0x0A0000 ${WORKING_DIR}/${SOC_RESCUE} ${JTAG_NOVERIFY}" >> ${JTAG_BATCH_FILE}
-	echo "flashmem 0x220000 ${WORKING_DIR}/${BIOS_RESCUE} ${JTAG_NOVERIFY}" >> ${JTAG_BATCH_FILE}
+	echo "flashmem 0x220000 ${BIOS_RESCUE_PATH}/${BIOS_RESCUE} ${JTAG_NOVERIFY}" >> ${JTAG_BATCH_FILE}
 	echo "flashmem 0x240000 ${WORKING_DIR}/${SPLASH_RESCUE} ${JTAG_NOVERIFY}" >> ${JTAG_BATCH_FILE}
 	echo "flashmem 0x2E0000 ${WORKING_DIR}/${FLICKERNOISE} ${JTAG_NOVERIFY}" >> ${JTAG_BATCH_FILE}
 
@@ -118,9 +131,9 @@ EOF
 	echo "flashmem 0x880000 ${WORKING_DIR}/${SPLASH} ${JTAG_NOVERIFY}" >> ${JTAG_BATCH_FILE}
 	echo "flashmem 0x920000 ${WORKING_DIR}/${FLICKERNOISE} ${JTAG_NOVERIFY}" >> ${JTAG_BATCH_FILE}
 
-	if [ "$1" == "--snapshot" ] && [ "$2" == "data" ] && [ -f "${WORKING_DIR}/${DATA}" ]; then
+	if [ -f "$2" ]; then
 	    echo "eraseflash 0xD20000 151" >> ${JTAG_BATCH_FILE}
-	    echo "flashmem   0xD20000 ${WORKING_DIR}/${DATA} ${JTAG_NOVERIFY}" >> ${JTAG_BATCH_FILE}
+	    echo "flashmem   0xD20000 $2 ${JTAG_NOVERIFY}" >> ${JTAG_BATCH_FILE}
 	fi
 
     fi
@@ -207,7 +220,7 @@ if [ "$1" == "--snapshot" ]; then
 	call-download
     fi
 
-    call-jtag $1 $3
+    call-jtag $1 "${WORKING_DIR}/${DATA}"
     exit 0
 fi
 
@@ -244,8 +257,50 @@ if [ "$1" == "--bios-mac" ]; then
 fi
 
 if [ "$1" == "--rc3" ]; then
-    echo "Not support yet!"
-    exit 1
+    if [ "$#" != "3" ]; then
+        call-help
+        exit 1
+    fi
+
+    MAC_DIR="BIOSMAC"
+    BIOS_RESCUE="bios-rescue-without-CRC.bin"
+    HEAD_TMP="head.tmp"
+    MAC_TMP="mac.tmp"
+    REMAIN_TMP="remain.tmp"
+    BIOS_RESCUE_MAC="bios.$2$3.bin"
+
+    mkdir -p ${MAC_DIR}
+
+    dd if=${BIOS_RESCUE} of=${MAC_DIR}/${HEAD_TMP}   bs=8 count=28
+    dd if=${BIOS_RESCUE} of=${MAC_DIR}/${REMAIN_TMP} bs=8  skip=29
+
+    printf "\\x$(printf "%x" 0x10)" >  ${MAC_DIR}/${MAC_TMP}
+    printf "\\x$(printf "%x" 0xe2)" >> ${MAC_DIR}/${MAC_TMP}
+    printf "\\x$(printf "%x" 0xd5)" >> ${MAC_DIR}/${MAC_TMP}
+    printf "\\x$(printf "%x" 0x00)" >> ${MAC_DIR}/${MAC_TMP}
+
+    printf "\\x$(printf "%x" 0x$2)" >> ${MAC_DIR}/${MAC_TMP}
+    printf "\\x$(printf "%x" 0x$3)" >> ${MAC_DIR}/${MAC_TMP}
+
+    printf "\\x$(printf "%x" 0x00)" >> ${MAC_DIR}/${MAC_TMP}
+    printf "\\x$(printf "%x" 0x00)" >> ${MAC_DIR}/${MAC_TMP}
+
+    cat ${MAC_DIR}/${HEAD_TMP} \
+        ${MAC_DIR}/${MAC_TMP} \
+        ${MAC_DIR}/${REMAIN_TMP} \
+        > ${MAC_DIR}/${BIOS_RESCUE_MAC}
+
+    mkmmimg ${MAC_DIR}/${BIOS_RESCUE_MAC} write
+
+    BIOS_RESCUE_PATH=${MAC_DIR}
+    BIOS_RESCUE=${BIOS_RESCUE_MAC}
+
+    WORKING_DIR=".."
+    FJMEM_PATH="."
+
+    call-jtag "--release" "${DATA}"
+
+    exit 0
 fi
 
 
